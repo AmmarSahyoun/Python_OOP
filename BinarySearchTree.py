84a258c079b12d665251cbdbb5628325727a1fbd
Ammar sahyoun <ammarzgr@hotmail.com>
2021-06-07 23:31:48 +0200

    complete the add_child func, and create in_order

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -76,11 +76,30 @@ class BinarySearchTreeNode:
     def add_child(self, data):
         if data == self.data:
             return
+
         if data < self.data:
             # add data in left subtree
-            pass
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
         else:
-            # add data in right subtree
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+    
+    def in_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        # visit base and right node
+
+    
+        return elements
+
 
             
 
