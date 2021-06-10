65c3d072a5944d8029df2c325358158231107402
Ammar sahyoun <ammarzgr@hotmail.com>
2021-06-10 18:32:48 +0200

    iterate in Dictionary

--- Iter_tuple_List_Dic_.py
+++ Iter_tuple_List_Dic_.py
@@ -276,96 +276,173 @@
 # print(len(t))
 # print(t)
 #-----------------------tic tac toe----------------------------
-from random import randrange
+# from random import randrange
 
-def display_board(board):
-	print("+-------" * 3,"+", sep="")
-	for row in range(3):
-		print("|       " * 3,"|", sep="")
-		for col in range(3):
-			print("|   " + str(board[row][col]) + "   ", end="")
-		print("|")
-		print("|       " * 3,"|",sep="")
-		print("+-------" * 3,"+",sep="")
+# def display_board(board):
+# 	print("+-------" * 3,"+", sep="")
+# 	for row in range(3):
+# 		print("|       " * 3,"|", sep="")
+# 		for col in range(3):
+# 			print("|   " + str(board[row][col]) + "   ", end="")
+# 		print("|")
+# 		print("|       " * 3,"|",sep="")
+# 		print("+-------" * 3,"+",sep="")
 
-def enter_move(board):
-	ok = False	# fake assumption - we need it to enter the loop
-	while not ok:
-		move = input("Enter your move: ") 
-		ok = len(move) == 1 and move >= '1' and move <= '9' # is user's input valid?
-		if not ok:
-			print("Bad move - repeat your input!") # no, it isn't - do the input again
-			continue
-		move = int(move) - 1 	# cell's number from 0 to 8
-		row = move // 3 	# cell's row
-		col = move % 3		# cell's column
-		sign = board[row][col]	# check the selected square
-		ok = sign not in ['O','X'] 
-		if not ok:	# it's occupied - to the input again
-			print("Field already occupied - repeat your input!")
-			continue
-	board[row][col] = 'O' 	# set '0' at the selected square
+# def enter_move(board):
+# 	ok = False	# fake assumption - we need it to enter the loop
+# 	while not ok:
+# 		move = input("Enter your move: ") 
+# 		ok = len(move) == 1 and move >= '1' and move <= '9' # is user's input valid?
+# 		if not ok:
+# 			print("Bad move - repeat your input!") # no, it isn't - do the input again
+# 			continue
+# 		move = int(move) - 1 	# cell's number from 0 to 8
+# 		row = move // 3 	# cell's row
+# 		col = move % 3		# cell's column
+# 		sign = board[row][col]	# check the selected square
+# 		ok = sign not in ['O','X'] 
+# 		if not ok:	# it's occupied - to the input again
+# 			print("Field already occupied - repeat your input!")
+# 			continue
+# 	board[row][col] = 'O' 	# set '0' at the selected square
 
-def make_list_of_free_fields(board):
-	free = []	# the list is empty initially
-	for row in range(3): # iterate through rows
-		for col in range(3): # iterate through columns
-			if board[row][col] not in ['O','X']: # is the cell free?
-				free.append((row,col)) # yes, it is - append new tuple to the list
-	return free
+# def make_list_of_free_fields(board):
+# 	free = []	# the list is empty initially
+# 	for row in range(3): # iterate through rows
+# 		for col in range(3): # iterate through columns
+# 			if board[row][col] not in ['O','X']: # is the cell free?
+# 				free.append((row,col)) # yes, it is - append new tuple to the list
+# 	return free
 
-def victory_for(board,sgn):
-	if sgn == "X":	# are we looking for X?
-		who = 'me'	# yes - it's computer's side
-	elif sgn == "O": # ... or for O?
-		who = 'you'	# yes - it's our side
-	else:
-		who = None	# we should not fall here!
-	cross1 = cross2 = True  # for diagonals
-	for rc in range(3):
-		if board[rc][0] == sgn and board[rc][1] == sgn and board[rc][2] == sgn:	# check row rc
-			return who
-		if board[0][rc] == sgn and board[1][rc] == sgn and board[2][rc] == sgn: # check column rc
-			return who
-		if board[rc][rc] != sgn: # check 1st diagonal
-			cross1 = False
-		if board[2 - rc][2 - rc] != sgn: # check 2nd diagonal
-			cross2 = False
-	if cross1 or cross2:
-		return who
-	return None
+# def victory_for(board,sgn):
+# 	if sgn == "X":	# are we looking for X?
+# 		who = 'me'	# yes - it's computer's side
+# 	elif sgn == "O": # ... or for O?
+# 		who = 'you'	# yes - it's our side
+# 	else:
+# 		who = None	# we should not fall here!
+# 	cross1 = cross2 = True  # for diagonals
+# 	for rc in range(3):
+# 		if board[rc][0] == sgn and board[rc][1] == sgn and board[rc][2] == sgn:	# check row rc
+# 			return who
+# 		if board[0][rc] == sgn and board[1][rc] == sgn and board[2][rc] == sgn: # check column rc
+# 			return who
+# 		if board[rc][rc] != sgn: # check 1st diagonal
+# 			cross1 = False
+# 		if board[2 - rc][2 - rc] != sgn: # check 2nd diagonal
+# 			cross2 = False
+# 	if cross1 or cross2:
+# 		return who
+# 	return None
 
-def draw_move(board):
-	free = make_list_of_free_fields(board) # make a list of free fields
-	cnt = len(free)
-	if cnt > 0:	# if the list is not empty, choose a place for 'X' and set it
-		this = randrange(cnt)
-		row, col = free[this]
-		board[row][col] = 'X'
+# def draw_move(board):
+# 	free = make_list_of_free_fields(board) # make a list of free fields
+# 	cnt = len(free)
+# 	if cnt > 0:	# if the list is not empty, choose a place for 'X' and set it
+# 		this = randrange(cnt)
+# 		row, col = free[this]
+# 		board[row][col] = 'X'
 
 
-board = [ [3 * j + i + 1 for i in range(3)] for j in range(3) ] # make an empty board
-board[1][1] = 'X' # set first 'X' in the middle
-free = make_list_of_free_fields(board)
-human_turn = True # which turn is it now?
-while len(free):
-	display_board(board)
-	if human_turn:
-		enter_move(board)
-		victor = victory_for(board,'O')
-	else:	
-		draw_move(board)
-		victor = victory_for(board,'X')
-	if victor != None:
-		break
-	human_turn = not human_turn		
-	free = make_list_of_free_fields(board)
+# board = [ [3 * j + i + 1 for i in range(3)] for j in range(3) ] # make an empty board
+# board[1][1] = 'X' # set first 'X' in the middle
+# free = make_list_of_free_fields(board)
+# human_turn = True # which turn is it now?
+# while len(free):
+# 	display_board(board)
+# 	if human_turn:
+# 		enter_move(board)
+# 		victor = victory_for(board,'O')
+# 	else:	
+# 		draw_move(board)
+# 		victor = victory_for(board,'X')
+# 	if victor != None:
+# 		break
+# 	human_turn = not human_turn		
+# 	free = make_list_of_free_fields(board)
 
-display_board(board)
-if victor == 'you':
-	print("You won!")
-elif victor == 'me':
-	print("I won")
-else:
-	print("Tie!")
+# display_board(board)
+# if victor == 'you':
+# 	print("You won!")
+# elif victor == 'me':
+# 	print("I won")
+# else:
+# 	print("Tie!")
+# ------------------------------------------------------------
 
+d={'one':[1,2,3], 'two':['Jonas', 'Lune', 'Sam']}
+
+# d.update({'one':5})
+# d.update({'three':['Ammar']})
+# print(d)
+#---------------Change key name ------------------------------
+# d['g'] = d['one']
+# del d['one']
+# print(d)
+#-----------------------Create dict from list---------------------------
+# names = ['Sam', 'Kale', 'Anna', 'Paul']
+# series1 = {names.index(x): x for x in (names)}
+# print(series1)
+#------------------------Create dict and get values as list---------------------------
+# f = {x: x*2 for x in range(5)}
+# print(f)
+# print([f.get(i) for i in f ])  # get values
+#--------------------Create Dict from list of names-------------------
+# my_dict = {}
+# names = ['Jonas', 'Lune', 'Pep']
+# for x in range(len(names)):
+#     my_dict[f"child{x}"] = names[x]
+# print(my_dict)
+#---------------loop through key and calues----------------------
+# colors = {
+#     "white": (255, 255, 255),
+#     "grey": (128, 128, 128),
+#     "red": (255, 0, 0),
+#     "green": (0, 128, 0)
+#     }
+# for col, rgb in colors.items():
+#     print(col, ":", rgb)
+#-------------------loop on Keys and print values---------------------------
+# d  ={}
+# d['1'] = [1,2]
+# d['2'] = [2,1]
+# for i in d.keys():
+#     print(d[i][1],end="")
+#---------------------Q----------------------
+# d = {'one':'two', 'three':'one','two':'three'}
+# v = d['three']
+# for k in range(len(d)):
+#     v = d[v]
+# print(v)
+#--------------------------
+#print(4**0.5)  #= 4 sqrt 
+#----------------------------
+# f = [i for i in range(-1,-2)]
+# print(f)
+#---------------------
+# l = [[x for x in range(3)] for y in range(3)]
+# for r in range(3):
+#     for c in range(3):
+#         if l[r][c] % 2 !=0:
+#             print('#')
+#-------------
+# m =[1,2]
+# for v in range(2):
+#     m.insert(-1,m[v])
+# print(m)
+#---------------------
+#print(1//5+1/5)
+#-----------------
+# x=1
+# y=2
+# x,y,z = x,x,y
+# z,y,z = x,y,z 
+# print(x,z,y)
+#--------------------
+# a = 1
+# b=0
+# a = a^b
+# b=a^b
+# a=a^b
+# print(a,b)
+#----------------
