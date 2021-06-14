bd0cbedaf443dbc766b7acd541aa7c095ac372f2
Ammar sahyoun <ammarzgr@hotmail.com>
2021-06-14 21:58:02 +0200

    add superhero Class

--- oop_Codia.py
+++ oop_Codia.py
@@ -10,3 +10,21 @@ class Person:
     def say_age(self):
         print(f"I am{self.age} years old.")
 
+class Superhero(Person):
+    def __init__(self, name, age, occupation, secret_identify, nemesis):
+        super().__init__(name, age, occupation)
+        self.secret_identify = secret_identify
+        self.nemesis = nemesis
+    def reveal_secret_identity(self):
+        print('the secret', self.secret_identify)
+    def say_nemesis(self):
+        print('My nemesis is', self.nemesis )
+    def say_hello(self):
+        print(f'I am {self.name}, and criminals fear me')
+    def say_age(self):
+        print('Young or old, I will triumph over evil.')
+    def old_say_hello(self):
+        super().say_hello()
+    def old_say_age(self):
+        super().say_age()
+
