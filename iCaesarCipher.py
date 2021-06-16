504cdea2d70c61edc3fcd79e4cb12f5bb5dc5831
Ammar sahyoun <ammarzgr@hotmail.com>
2021-06-16 22:39:23 +0200

    Add decrypt function

--- iCaesarCipher.py
+++ iCaesarCipher.py
@@ -1,14 +1,27 @@
 # Encrypt:
-text = input("Enter your message: ")
-cipher = ''
-for char in text:
+# text = input("Enter your message: ")
+# cipher = ''
+# for char in text:
+#     if not char.isalpha():
+#         continue
+#     char = char.upper()
+#     code = ord(char)+1
+#     if code > ord('Z'):
+#         code = ord('A')
+#     cipher += chr(code)
+# print(cipher)
+
+# decrypt:
+
+cipher = input('Enter your cryptogram: ')
+text = ''
+for char in cipher:
     if not char.isalpha():
         continue
     char = char.upper()
-    code = ord(char)+1
-    if code > ord('Z'):
-        code = ord('A')
-    cipher += chr(code)
-print(cipher)
+    code = ord(char) - 1
+    if code < ord('A'):
+        code = ord('Z')
+    text += chr(code)
 
-# decrypt:
+print(text)
