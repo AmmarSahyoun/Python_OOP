34d2e6c917a1c780e0f9e8e714e3bd0f280e04c7
Ammar sahyoun <ammarzgr@hotmail.com>
2021-06-21 22:41:22 +0200

    Create coffee journal class

---
+++ coffeeJournal.py
@@ -0,0 +1,22 @@
+import csv
+
+class CoffeeJournal:
+    def __init__(self, file):
+        self._file = file
+        self._roaster = ""
+        self._country = ""
+        self._region = ""
+        self._stars = ""
+        self._new_coffee = []
+        self._old_coffee = self.load_coffee()
+
+    def load_coffee(self):
+        coffee = []
+        with open(self._file) as f:
+            reader = csv.reader(f, delimiter=',')
+            for row in reader:
+                coffee.append(row)
+        return coffee
+
+test_object = CoffeeJournal("test_journal1.csv")
+print(test_object._old_coffee)
