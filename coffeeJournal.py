52c4f717a5a792578d4e89b021bf83dd9de0d7b3
Ammar sahyoun <ammarzgr@hotmail.com>
2021-06-22 12:32:25 +0200

    Create show coffee and test

--- coffeeJournal.py
+++ coffeeJournal.py
@@ -49,15 +49,41 @@ class CoffeeJournal:
         with open(self._file, 'a') as f: #in append mode
             writer = csv.writer(f)
             writer.writerows(self._new_coffee)
+    
+    def show_coffee(self):
+        print()
+        # if there is no information on any coffee, tell the user to add one
+        if len(self._old_coffee) < 2 and len(self._new_coffee) == 0:
+            print("Enter a coffee first")
+        # if there is information in the CSV but not new coffee print the old coffee
+        elif len(self._old_coffee) > 2 and len(self._new_coffee) == 0:
+            for row in self._old_coffee:
+                print(f"{row[0]:13} {row[1]:13} {row[2]:13}  {row[3]:13}")
+        # print both the old coffee and the new coffee
+        else:
+            for row in self._old_coffee:
+                print(f"{row[0]:13} {row[1]:13} {row[2]:13}  {row[3]:13}")
+            for row in self._new_coffee:
+                print(f"{row[0]:13} {row[1]:13} {row[2]:13}  {row[3]:13}")
+        print()
 
-test_object = CoffeeJournal("test_journal1.csv")
-print(test_object._old_coffee)
-test_object = CoffeeJournal("test_journal1.csv")
-test_object.roaster = "Peace River"
-test_object.country = "Rawanda"
-test_object.region = "Remera"
-test_object.stars = "***"
-print(test_object.roaster)
-print(test_object.country)
-print(test_object.region)
-print(test_object.stars)
+
+# **********************************************
+# code for testing your script
+# **********************************************
+
+test_object2 = CoffeeJournal("test_journal1.csv")
+test_object2.show_coffee()
+
+
+# test_object = CoffeeJournal("test_journal1.csv")
+# print(test_object._old_coffee)
+# test_object = CoffeeJournal("test_journal1.csv")
+# test_object.roaster = "Peace River"
+# test_object.country = "Rawanda"
+# test_object.region = "Remera"
+# test_object.stars = "***"
+# print(test_object.roaster)
+# print(test_object.country)
+# print(test_object.region)
+# print(test_object.stars)
