6980d7cf9e42873f2a751e7f75c5e9b088599c26
Ammar sahyoun <ammarzgr@hotmail.com>
2021-06-23 23:34:08 +0200

    create stats equations

--- readStatis.py
+++ readStatis.py
@@ -5,4 +5,14 @@ class Statistics:
         self._variance = 0.
         self.mean()
         self.std()
- 
+    def mean(self):
+        self._mu = sum(self._newlist)/len(self._newlist)
+        print("mean: ", self._mu)
+        return self._mu
+    def std(self):
+        w = [(i-self._mu)**2 for i in self._newlist]
+        self._variance = (1/len(self._newlist))*sum(w)
+        print("std: ", round(math.sqrt(self._variance),4))
+
+
+letsGo = Statistics(mydata)
