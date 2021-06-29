67d119d856a2a4139de8ff853ca515d781fd4030
Ammar sahyoun <ammarzgr@hotmail.com>
2021-06-29 10:16:40 +0200

    create information class to store data

--- contactList.py
+++ contactList.py
@@ -1,6 +1,6 @@
 class Contacts:
   def __init__(self):
-    self.view = 'quit'
+    self.view = 'list'
     self.contact_list = []
     self.choice = None
     self.index = None
@@ -17,12 +17,32 @@ class Contacts:
         print('\nClosing the contact list...\n')
         break
     def show_list(self):
-        pass
-
+        print()
+        if len(self.contact_list) == 0:
+            self.choice = input('(A)dd a new contact \n(Q)uit \n> ').lower()
+        else:
+            self.view = 'quit'
+            self.handle_choice()
     def show_info(self):
         pass
 
     def add_contact(self):
         pass
+
+    def handle_choice(self):
+        if self.choice == 'q':
+            self.choice = ' quit'
+        elif self.choice == 'a' and self.view =='list':
+            self.view = 'add'
+class Information:
+  def __init__(self):
+    self.first_name = input('Enter their first name: ')
+    self.last_name = input('Enter their last name: ')
+    self.personal_phone = input('Enter their personal phone number: ')
+    self.personal_email = input('Enter their personal email address: ')
+    self.work_phone = input('Enter their work phone number: ')
+    self.work_email = input('Enter their work email address: ')
+    self.title = input('Enter their work title: ')
+
 contacts = Contacts()
 contacts.display()
