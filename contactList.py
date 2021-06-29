e8b3ab1be5316a22f31837e75306b9177e2de9aa
Ammar sahyoun <ammarzgr@hotmail.com>
2021-06-29 10:05:37 +0200

    create functions

--- contactList.py
+++ contactList.py
@@ -16,3 +16,13 @@ class Contacts:
       elif self.view == 'quit':
         print('\nClosing the contact list...\n')
         break
+    def show_list(self):
+        pass
+
+    def show_info(self):
+        pass
+
+    def add_contact(self):
+        pass
+contacts = Contacts()
+contacts.display()
