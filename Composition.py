57d1d518b2d437d3cf7c08db00f2793d3a656a70
Ammar sahyoun <ammarzgr@hotmail.com>
2021-07-07 23:35:14 +0200

    Create Engine class

--- Composition.py
+++ Composition.py
@@ -8,3 +8,13 @@ class Car:
         print(f'{self.year} {self.make} {self.model}')
     def start(self):
         self.engine.ignite()
+class Engine:
+    def __init__(self, configuration, displacement, horsepower, torque):
+        self.configuration = configuration
+        self.displacement = displacement
+        self.horsepower = horsepower
+        self.torque = torque
+    def ignite(self):
+        print('Vroom, vroom!')
+    def info(self): # the composition is a one way street, the engine class has no access
+        self.describe() # to component class Car! But the Car has access to attr&meth on Engine class
