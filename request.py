d5f9d3c7a6697d516927202b68eb04bf7e5dd7fa
Ammar sahyoun <ammarzgr@hotmail.com>
2021-08-04 20:21:00 +0200

    Add request

---
+++ request.py
@@ -0,0 +1,17 @@
+import requests
+
+api_url = "https://jsonplaceholder.typicode.com/todos/1"
+response = requests.get(api_url)
+print(response.json())
+print(type(response))
+
+#-----------------------------
+
+response = requests.get('https://api.github.com/search/repositories',
+ params={'q': 'requests+language:python'},)
+
+# Inspect some attributes of the `requests` repository
+json_response = response.json()
+repository = json_response['items'][0]
+print(f'Repository name: {repository["name"]}') 
+print(f'Repository description: {repository["description"]}')  
