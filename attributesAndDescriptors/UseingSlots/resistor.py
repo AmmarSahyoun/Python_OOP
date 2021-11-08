dbe1206a4583e0016bc1efdf121c934385715d3b
Ammar sahyoun <ammarzgr@hotmail.com>
2021-11-08 10:58:22 +0100

    Add slot to decrease size

--- attributesAndDescriptors/UseingSlots/resistor.py
+++ attributesAndDescriptors/UseingSlots/resistor.py
@@ -1,8 +1,11 @@
 class Resistor:
 
+    """Using slot can reduce the size to the half but in the same time it has a trade-off to
+        no longer dynamically add attributes to instances"""
+    __slots__ = ["resistance_ohms", "tolerance_percent", "power_watts"]
+
     def __init__(self, resistance_ohms, tolerance_percent, power_watts):
         self.resistance_ohms = resistance_ohms
         self.tolerance_percent = tolerance_percent
         self.power_watts = power_watts
-
 
