6f0bb9a0ae937b4fd22bf5f6503697b5618afb49
Ammar sahyoun <ammarzgr@hotmail.com>
2021-11-08 10:17:03 +0100

    override repr function

--- attributesAndDescriptors/loggingproxy.py
+++ attributesAndDescriptors/loggingproxy.py
@@ -20,3 +20,23 @@ class LoggingProxy:
         print(f"Retrieved attribute {name} == {value!r} from {target!r}")
         return value
 
+    """ Intercept attribute writes """
+    def __setattr__(self, name, value):
+        target = super().__getattribute__("target")
+        try:
+            setattr(target, name, value)
+        except AttributeError as e:
+            raise AttributeError(
+                "{} could not forward request {} to {}".format(
+                    super().__getattribute__("__class__").__name__,
+                    name,
+                    target
+                )
+            ) from e
+        print(f"Set attribute {name} == {value!r} on {target!r}")
+
+    """ Get the string representation of the target ColoredVector rather than LoggingProxy"""
+    def __repr__(self):
+        target = super().__getattribute__("target")
+        repr_callable = getattr(target, "__repr__")
+        return repr_callable()
