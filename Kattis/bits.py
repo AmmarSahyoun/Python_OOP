83da9648458d73b689c288416d32af79d776282a
Ammar sahyoun <ammarzgr@hotmail.com>
2022-01-08 21:23:09 +0100

    Add bits

---
+++ Kattis/bits.py
@@ -0,0 +1,11 @@
+#bits
+
+inp = int(input())
+
+for _ in range(inp):
+    binary = input()
+    bits = 0
+    for i in range(1, len(binary) + 1):
+        check = int(binary[:i])
+        bits = max(bits, bin(check).count('1'))
+    print (bits)
